77baa653a86fa7aa8b5af947379d2132ed7360cd
김석환 <gworldkim@gmail.com>
2024-01-10 12:28:20 +0900

    merge main & working news

--- apps/news/models.py
+++ apps/news/models.py
@@ -8,6 +8,18 @@ class NewsHome(AbstractPageHome):
 
     subpage_types = ["News"]
 
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request)
+        newspages = self.get_children().live().order_by("-first_published_at")
+        context["newspages"] = newspages
+        return context
+
+    def get_template(self, request, *args, **kwargs):
+        return "news/news_home.html"
+
+    def get_landing_page_template(self, *args, **kwargs):
+        return self.get_template(*args, **kwargs)
+
 
 class News(AbstractDetailPage):
     class Meta:
@@ -16,3 +28,9 @@ class News(AbstractDetailPage):
 
     parent_page_types = ["NewsHome"]
     subpage_types = []
+
+    def get_template(self, request, *args, **kwargs):
+        return "news/news_page.html"
+
+    def get_landing_page_template(self, *args, **kwargs):
+        return self.get_template(*args, **kwargs)
