2a616b1085d271e3ea38fc67bd8f77fff2c21071
김석환 <gworldkim@gmail.com>
2024-01-15 11:17:09 +0900

    edit news templates

--- apps/news/models.py
+++ apps/news/models.py
@@ -1,3 +1,7 @@
+from django.db.models import CASCADE, ForeignKey
+from modelcluster.fields import ParentalKey
+from wagtail.admin.panels import FieldPanel, InlinePanel
+from wagtail.models import Orderable
 from apps.base.models import AbstractPageHome, AbstractDetailPage
 
 
@@ -16,9 +20,10 @@ class NewsHome(AbstractPageHome):
 
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
-        context['newspages'] = self.get_children().live().order_by("-id")[0:6]
+        context["newspages"] = self.get_children().live().order_by("-id")
         return context
 
+    # TODO // Pagination "더보기"
 
 class News(AbstractDetailPage):
     class Meta:
@@ -28,8 +33,28 @@ class News(AbstractDetailPage):
     parent_page_types = ["NewsHome"]
     subpage_types = []
 
+    content_panels = AbstractDetailPage.content_panels + [
+        InlinePanel("images", label="이미지"),
+    ]
+
     def get_template(self, request, *args, **kwargs):
         return "news/news_page.html"
 
     def get_landing_page_template(self, *args, **kwargs):
         return self.get_template(*args, **kwargs)
+
+    def main_image(self):
+        gallery_item = self.images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
+
+
+class NewsImage(Orderable):
+    news = ParentalKey("News", on_delete=CASCADE, related_name="images")
+    image = ForeignKey("wagtailimages.Image", on_delete=CASCADE, related_name="+")
+
+    panels = [
+        FieldPanel("image"),
+    ]
